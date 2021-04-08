29f27e020a769e259d041f520ef1d338a0ce1bbb
stachowski <stachowskibartosz96@gmail.com>
2021-04-08 19:33:15 +0200

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './assets/tailwind.css';
+import AppButton from './components/AppButton.vue';
+
+Vue.component('AppButton', AppButton);
 
 Vue.config.productionTip = false;
 
