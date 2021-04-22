d90c5b3e2f48eb183e3b9ccdfeabbcc95d3da6d7
stachowski <stachowskibartosz96@gmail.com>
2021-04-22 21:23:56 +0200

    refactor - add drag and drop components

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,43 +1,50 @@
 <template>
-  <div
-    class="column"
-    draggable
-    @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
-    @dragover.prevent
-    @dragenter.prevent
-    @dragstart.self="pickupColumn($event, columnIndex)"
-  >
-    <div class="flex items-center mb-2 font-bold">
-      {{column.name}}
-    </div>
-    <div class="list-reset">
-      <ColumnTask
-        v-for="(task, $taskIndex) of column.tasks"
-        :key="$taskIndex"
-        :task="task"
-        :taskIndex="$taskIndex"
-        :column="column"
-        :columnIndex="columnIndex"
-        :board="board"
-      />
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="column"
+      :transferData="{
+        type: 'column',
+        fromColumnIndex: columnIndex
+      }"
+    >
+      <div class="flex items-center mb-2 font-bold">
+        {{column.name}}
+      </div>
+      <div class="list-reset">
+        <ColumnTask
+          v-for="(task, $taskIndex) of column.tasks"
+          :key="$taskIndex"
+          :task="task"
+          :taskIndex="$taskIndex"
+          :column="column"
+          :columnIndex="columnIndex"
+          :board="board"
+        />
 
-      <input
-        type="text"
-        class="block p-2 w-full bg-transparent"
-        placeholder="+ Enter new task"
-        @keyup.enter="createTask($event, column.tasks)"
-      />
-    </div>
-  </div>
+        <input
+          type="text"
+          class="block p-2 w-full bg-transparent"
+          placeholder="+ Enter new task"
+          @keyup.enter="createTask($event, column.tasks)"
+        />
+      </div>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import movingTaskAndColumnMixin from '@/mixins/movingTaskAndColumnMixin';
+import AppDrag from './AppDrag.vue';
+import AppDrop from './AppDrop.vue';
 import ColumnTask from './ColumnTask.vue';
 
 export default {
   name: 'BoardColumn',
-  components: { ColumnTask },
+  components: {
+    ColumnTask,
+    AppDrag,
+    AppDrop,
+  },
   mixins: [movingTaskAndColumnMixin],
   methods: {
     pickupColumn(e, fromColumnIndex) {
